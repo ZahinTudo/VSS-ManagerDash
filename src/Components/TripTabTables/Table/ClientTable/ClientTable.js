bc4dbb91925f5937284e01ffcae5ab2e9bf13afc
zahin52 <zahinuddin5@gmail.com>
2022-08-13 03:31:23 +0600

    edited the clientsliding window

--- src/Components/TripTabTables/Table/ClientTable/ClientTable.js
+++ src/Components/TripTabTables/Table/ClientTable/ClientTable.js
@@ -8,92 +8,119 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Table } from "react-bootstrap";
+import ClientDetailsSlidingWindow from "./ClientDetailsSlidingWindow/ClientDetailsSlidingWindow";
+import { useAnimation } from "framer-motion";
 export default function ClientTable({ activeTrip }) {
+	const ClientDetailsAnimation = useAnimation();
+	const openClientDetailsSlidingWindow = () => {
+		ClientDetailsAnimation.start({
+			x: 0,
+			transition: {
+				duration: 0.15,
+			},
+		});
+	};
 	return (
-		<Table responsive hover className='table-borderless'>
-			<thead>
-				<tr>
-					<th>
-						<input
-							className='largerCheckbox'
-							type='checkbox'
-							name=''
-							id=''
-						/>
-					</th>
-					{activeTrip?.tableHeads.map((item, index) => (
-						<th key={index}>{item}</th>
-					))}
-				</tr>
-			</thead>
-			<tbody>
-				{activeTrip?.tableData.map((item, index) => (
+		<div className='clientTable'>
+			<ClientDetailsSlidingWindow
+				ClientDetailsAnimation={ClientDetailsAnimation}
+			/>
+			<Table responsive hover className='table-borderless'>
+				<thead>
 					<tr>
-						<td style={{ verticalAlign: "middle" }}>
+						<th>
 							<input
 								className='largerCheckbox'
 								type='checkbox'
 								name=''
-								id={index}
+								id=''
 							/>
-						</td>
-						{Object.entries(item).map(([key, value], index) => (
-							<td key={index} style={{ verticalAlign: "middle" }}>
-								<span className='d-flex align-items-center'>
-									{(function () {
-										switch (key) {
-											case "State & City":
-												return (
-													<div className='d-flex flex-column'>
-														<span>
-															{value.state}
+						</th>
+						{activeTrip?.tableHeads.map((item, index) => (
+							<th key={index}>{item}</th>
+						))}
+					</tr>
+				</thead>
+				<tbody>
+					{activeTrip?.tableData.map((item, index) => (
+						<tr>
+							<td style={{ verticalAlign: "middle" }}>
+								<input
+									className='largerCheckbox'
+									type='checkbox'
+									name=''
+									id={index}
+								/>
+							</td>
+							{Object.entries(item).map(([key, value], index) => (
+								<td
+									key={index}
+									style={{ verticalAlign: "middle" }}>
+									<span className='d-flex align-items-center'>
+										{(function () {
+											switch (key) {
+												case "State & City":
+													return (
+														<div className='d-flex flex-column'>
+															<span>
+																{value.state}
+															</span>
+															<span>
+																{value.city}
+															</span>
+														</div>
+													);
+												case "Client Company":
+													return (
+														<span
+															className='link'
+															onClick={
+																openClientDetailsSlidingWindow
+															}>
+															{" "}
+															{value}
 														</span>
-														<span>
-															{value.city}
-														</span>
-													</div>
-												);
-											case "Client Company":
-												return <a href=''> {value}</a>;
+													);
 
-											default:
-												return value;
-										}
-									})()}
+												default:
+													return value;
+											}
+										})()}
+									</span>
+								</td>
+							))}
+
+							<td
+								style={{
+									verticalAlign: "middle",
+									width: "max-content",
+									display: "table-cell",
+								}}
+								className='align-items-center text-dark'>
+								<span className='actionIcons'>
+									<FontAwesomeIcon icon={faPencil} />
+								</span>
+								<span className='actionIcons'>
+									<FontAwesomeIcon icon={faTrash} />
+								</span>
+								<span className='actionIcons'>
+									<FontAwesomeIcon icon={faCommenting} />
+								</span>
+								<span className='actionIcons'>
+									<FontAwesomeIcon icon={faMapMarkerAlt} />
+								</span>
+								<span className='actionIcons'>
+									<img
+										src='/assets/sosIcon.svg'
+										alt=''
+										className='img-fluid'
+									/>
 								</span>
 							</td>
-						))}
-
-						<td
-							style={{
-								verticalAlign: "middle",
-								width: "max-content",
-								display: "table-cell",
-							}}
-							className='align-items-center text-dark'>
-							<span className='actionIcons'>
-								<FontAwesomeIcon icon={faPencil} />
-							</span>
-							<span className='actionIcons'>
-								<FontAwesomeIcon icon={faTrash} />
-							</span>
-							<span className='actionIcons'>
-								<FontAwesomeIcon icon={faCommenting} />
-							</span>
-							<span className='actionIcons'>
-								<FontAwesomeIcon icon={faMapMarkerAlt} />
-							</span>
-							<span className='actionIcons'>
-								<img
-									src='/assets/sosIcon.svg'
-									alt=''
-									className='img-fluid'
-								/>
-							</span>
-						</td>
-					</tr>
-				))}
-			</tbody>
-		</Table>
+						</tr>
+					))}
+				</tbody>
+			</Table>
+		</div>
 	);
 }
